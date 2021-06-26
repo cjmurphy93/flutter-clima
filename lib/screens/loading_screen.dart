da111de802a7b9753581c537485553c1aa36a385
cjmurphy93 <cjmurph93@gmail.com>
2021-06-26 14:39:30 -0400

    added updated code for http.get

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -17,6 +18,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = Location();
     await location.getCurrentLocation();
+  }
+
+  void getData() async {
+    var uriExample = Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=439d4b804bc8187953eb36d2a8c26a02');
+
+    http.Response response = await http.get(uriExample);
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var jsonResponse = convert.jsonDecode(response.body);
+      print(jsonResponse);
+    } else {
+      print(response.statusCode);
+    }
   }
 
   @override
